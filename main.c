1db34cd24ccf81475774b2b593a387b75e496417
CandyMi <869646063@qq.com>
2025-11-17 16:46:19 +0800

    test: 完善测试用例
    Updated socket initialization and adjusted buffer size.

--- main.c
+++ main.c
@@ -5,9 +5,11 @@
 
 int main(int argc, char const *argv[])
 {
+  //SOCKET sock = socket(AF_INET, SOCK_STREAM, IPPROTO_IP);
+
   ccsocket_t sock = ccsocket(CC_INET4, CC_TCP);
-  // ccsocket_t sock = ccsocket1(CC_INET4, CC_TCP, CC_NONBLOCK);
-  printf("new fd = %d\n", sock);
+  //ccsocket_t sock = ccsocket1(CC_INET4, CC_TCP, CC_NONBLOCK);
+  printf("new fd = %ld\n", sock);
 
   int rc = ccsocket_connect(sock, "120.24.216.230", 80);
   // int rc = ccsocket_connect(sock, "127.0.0.1", 7888);
@@ -17,10 +19,11 @@ int main(int argc, char const *argv[])
   int rs = ccsocket_send(sock, req, strlen(req));
   printf("send r = %d\n", rs);
 
-  char buf[2048];
+  char buf[1024]; memset(buf, 0x0, 1024);
   int rr = ccsocket_recv(sock, buf, 1024);
   printf("recv r = %d, ['%s']\n", rr, buf);
 
   printf("close fd = %d\n", ccsocket_close(sock));
+
   return 0;
 }
