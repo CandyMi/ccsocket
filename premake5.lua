abf6350862f57270a96e9b0af5488bb94633b662
869646063@qq.com <869646063@qq.com>
2025-12-06 20:30:00 +0800

    build: 增加premake5的构建支持

---
+++ premake5.lua
@@ -0,0 +1,61 @@
+---@diagnostic disable: undefined-global
+
+workspace "ccsocket"
+  configurations { "Debug", "Release" }
+
+local function ccbuild (
+  project_name, project_type,
+  project_language, project_files,
+  project_links, project_filename,
+  preject_output
+)
+  project(project_name)
+    kind(project_type)
+    language(project_language)
+    files(project_files)
+
+    targetdir "build"
+    if preject_output then
+      location(preject_output)
+      targetdir(preject_output)
+    end
+
+    if project_links then
+      links(project_links)
+      libdirs { 'build' }
+    end
+
+    if project_filename then
+      targetname(project_filename)
+    end
+
+    filter "configurations:Debug"
+      defines { "DEBUG" }
+      symbols "On"
+      optimize "Off"
+
+    filter "configurations:Release"
+      defines { "NDEBUG" }
+      optimize "On"
+
+    filter "system:windows"
+      targetprefix ""
+end
+
+ccbuild(
+  'ccsocket-dynamic', 'SharedLib', 'C',
+  {'ccsocket.c', 'ccsocket.h'},
+  nil, 'ccsocket', 'build'
+)
+
+ccbuild(
+  'ccsocket-static', 'StaticLib', 'C',
+  {'ccsocket.c', 'ccsocket.h'},
+  nil, 'ccsocket', 'build'
+)
+
+ccbuild(
+  'testmain', 'ConsoleApp', 'C',
+  {'main.c', 'ccsocket.h'},
+  {'ccsocket'}, 'main', 'build'
+)
