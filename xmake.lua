8d3743b7b38aafc2c80627351e68a8f5fef58124
869646063@qq.com <869646063@qq.com>
2025-12-07 10:46:07 +0800

    build: 优化xmake构建

--- xmake.lua
+++ xmake.lua
@@ -2,25 +2,29 @@
 
 add_rules("mode.debug", "mode.release")
 
+local stdc = "c99"
+local stdcxx = "c++11"
+local output = "build"
+
 -- 生成.o文件
 target("ccsocket-object")
     set_kind("object")
     add_files("ccsocket.c")
-    set_languages('c89')
-    set_targetdir("build")
+    set_languages(stdc)
+    set_targetdir(output)
 
 -- 动态库
 target("ccsocket-dynamic")
     set_kind("shared")
     add_deps("ccsocket-object")
-    set_targetdir("build")
+    set_targetdir(output)
     set_basename("ccsocket")
 
 -- 静态库
 target("ccsocket-static")
     set_kind("static")
     add_deps("ccsocket-object")
-    set_targetdir("build")
+    set_targetdir(output)
     set_basename("ccsocket")
 
 -- 测试文件
@@ -28,8 +32,8 @@ target("testmain")
     set_kind("binary")
     add_files("main.c")
     add_deps("ccsocket-dynamic", "ccsocket-static")
-    add_rpathdirs("build")
-    add_linkdirs("build")
     add_links('ccsocket')
-    set_targetdir("build")
+    add_rpathdirs(output)
+    add_linkdirs(output)
+    set_targetdir(output)
     set_basename("main")
